099aad9e7bec6cdc98e586c656b242db1f5f5154
boeyjw-erc-hpc-login2.create.kcl.ac.uk <23478480+boeyjw@users.noreply.github.com>
2023-02-15 00:40:34 +0000

    upd: preproc scifact and fix read gzip read gzip now returns a list of docs instead of a list of 1 element which contains list of docs

--- nb/src/gen/util.py
+++ nb/src/gen/util.py
@@ -4,7 +4,7 @@ import json
 def read_gzip_data(fp):
     with gzip.GzipFile(fp, "r") as gzfn:
         data = [json.loads(l.decode("utf8")) for l in gzfn.readlines()]
-    return data
+    return data if len(data) > 1 else data[0]
 
 def write_gzip_data(fp, payload):
     with gzip.GzipFile(fp, "w") as gzfn:
