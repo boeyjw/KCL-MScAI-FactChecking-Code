90d64fa7ca4f13b3c0db00e5a88a4de298b3e972
boeyjw-erc-hpc-login2.create.kcl.ac.uk <23478480+boeyjw@users.noreply.github.com>
2023-07-20 21:32:38 +0100

    upd: error processing done for scifact oracle ir

--- nb/src/gen/special.py
+++ nb/src/gen/special.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def entropy3(labels, base=np.e):
+def entropy3(labels, base=3):
     # Reference: https://gist.github.com/jaradc/eeddf20932c0347928d0da5a09298147
+    # Use base 3 by default since there are 3 labels
     vc = pd.Series(labels).value_counts(normalize=True, sort=False)
     return -(vc * np.log(vc)/np.log(base)).sum()
